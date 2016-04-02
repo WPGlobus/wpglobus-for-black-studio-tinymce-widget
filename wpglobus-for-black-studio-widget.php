5d7a522e54116356941ac624674d5f6ccbf3a0a6
Alex Gor <alex.wpstarter@gmail.com>
2016-04-02 19:45:43 +0700

    Version bump

--- wpglobus-for-black-studio-widget.php
+++ wpglobus-for-black-studio-widget.php
@@ -5,7 +5,7 @@
  * Description: WPGlobus add-on for Black Studio TinyMCE Widget
  * Text Domain: 
  * Domain Path: 
- * Version: 1.0.1
+ * Version: 1.1.0
  * Author: WPGlobus
  * Author URI: http://www.wpglobus.com/
  * Network: false
@@ -39,7 +39,7 @@ if ( class_exists('Black_Studio_TinyMCE_Plugin') ) {
 			return;
 		}
 		
-		define( 'WPGLOBUS_BS_WIDGET', '1.0.1' );
+		define( 'WPGLOBUS_BS_WIDGET', '1.1.0' );
 
 		require_once( 'class-wpglobus-for-black-studio-widget.php' );
 
